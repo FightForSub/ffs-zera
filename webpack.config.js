e1fa78bc828d56830afb4813f418d303977dcd89
WilliamDASILVA <william.da.silva@outlook.com>
2017-11-14 01:02:21 +0100

    Add @ alias

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const baseConfig = require('webpack-focus/config/default');
 const envParser = require('webpack-focus/webpack-utilities/env-parser');
 
@@ -6,5 +7,6 @@ const parsedEnv = envParser(process.env);
 myConfig.addDefinedVariable('__CLIENT_ID__', JSON.stringify(parsedEnv.CLIENT_ID));
 myConfig.addDefinedVariable('__BASE_URL__', JSON.stringify(parsedEnv.BASE_URL));
 myConfig.addDefinedVariable('__ROOT_URL__', JSON.stringify(parsedEnv.ROOT_URL));
+myConfig.addAlias('@', path.join(__dirname, 'app'));
 
 module.exports = myConfig.toWebpackConfig(parsedEnv);

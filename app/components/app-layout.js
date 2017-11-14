b088a407c3594eeebf4e3ead38957f061eb032cb
Hartorn <bazirehoussin@gmail.com>
2017-11-14 08:56:03 +0100

    Adding back devtools and loading bar

--- app/components/app-layout.js
+++ app/components/app-layout.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 import AppSideMenu from '@/components/app-side-menu';
 import AppContent from '@/components/app-content';
+import DevTools from '@/components/dev-tools';
+import LoadingBar from 'focus-components/components/layout/header-loading-bar';
 
 const CustomLayout = (props) => (
     <div className='app-layout'>
+        <LoadingBar />
         <AppSideMenu />
         <AppContent>
             {props.children}
         </AppContent>
+        {__DEV__ && <DevTools />}
     </div >
 );
 CustomLayout.displayName = 'CustomAppLayout';

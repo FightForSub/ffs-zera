07b2ca3a0686f38ef4c4d53fc053dd16357a1958
Hartorn <bazirehoussin@gmail.com>
2017-11-14 08:51:01 +0100

    Alias is relative

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const baseConfig = require('webpack-focus/config/default');
 const envParser = require('webpack-focus/webpack-utilities/env-parser');
 
@@ -7,6 +6,6 @@ const parsedEnv = envParser(process.env);
 myConfig.addDefinedVariable('__CLIENT_ID__', JSON.stringify(parsedEnv.CLIENT_ID));
 myConfig.addDefinedVariable('__BASE_URL__', JSON.stringify(parsedEnv.BASE_URL));
 myConfig.addDefinedVariable('__ROOT_URL__', JSON.stringify(parsedEnv.ROOT_URL));
-myConfig.addAlias('@', path.join(__dirname, 'app'));
+myConfig.addAlias('@', './app');
 
 module.exports = myConfig.toWebpackConfig(parsedEnv);
